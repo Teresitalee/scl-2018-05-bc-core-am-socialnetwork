3d61908d283586f8192175dbc31c7ebf80ec84e8
Natalia <nataliamp87@gmail.com>
2018-07-17 22:35:01 -0400

    imprime usuario nombre y foto de google en muro

--- assets/js/muro.js
+++ assets/js/muro.js
@@ -1,58 +1,25 @@
 
-
-//Avatar y nombre GOOGLE
-function InicializarFire() {
-
-
-  firebase.auth().onAuthStateChanged(function (user) {
-    /* body... */
-
-    if (user) {
-      var userDisplayName = user.displayName;
-      var userPhoto = user.photoURL;
-
-      userName.textContent = userDisplayName;
-      userPic.style.backgroundImage = 'url(' + userPhoto + ')';
-
-
-    }
-
-  });
-
-}
-
-window.onload = function () {
-  InicializarFire();
-}
+// Avatar y nombre GOOGLE
 
 var userPic = document.getElementById('user-pic');
 var userName = document.getElementById('user-name');
 
+function InicializarFire() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    /* body... */
 
-//Publicar
-const boton = document.getElementById("btnPublicar");
+    if (user) {
+      let userDisplayName = user.displayName;
+      let userPhoto = user.photoURL;
 
-boton.addEventListener("click", () => {
-  // Acá guardo el comentario ingresado por el usuario en el textarea
-  let comments = document.getElementById("comment").value;
+      userName.textContent = userDisplayName;
+      userPic.style.backgroundImage = 'url(' + userPhoto + ')';
+    }
+  });
+}
 
-  // Limpiar el textarea
-  document.getElementById("comment").value = " ";
-
-  //contenedor donde dejaré mis comentarios en HTML
-  const cont = document.getElementById("cont");
-
-  //Crear div contenedor
-  const newComments = document.createElement("div");
-
-  //Validar que el textarea tenga un comentario y no esté vacío
-  if (comments.length === 0 || comments === null){
-    alert ("Debes ingresar un mensaje");
-    return false;
-  }
-
-})
-
-
+window.onload = function() {
+  InicializarFire();
+};
 
 
