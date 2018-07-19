ca24a71348192f457d158cb7196cdaecbd8d4f24
Natalia <nataliamp87@gmail.com>
2018-07-19 18:14:20 -0400

    funciona editar pero duplica mensaje. Aparece nombre en post y la fecha

--- assets/js/database.js
+++ assets/js/database.js
@@ -43,7 +43,7 @@ db.collection("messages").onSnapshot((querySnapshot) => {
     <p>${doc.data().text}</p>
     <div class="icon">
     <button class="btn-delete" onclick="eliminar('${doc.id}')" id="icon-post"><i class="fas fa-trash-alt iconPost""></i></button>
-    <button class="btn-edit" onclick="editar('${doc.creator}', '${doc.data().text}')"><i class="fas fa-edit iconPost""></i></button>
+    <button class="btn-edit" onclick="editar('${doc.id}', '${doc.data().text}')"><i class="fas fa-edit iconPost""></i></button>
     <button class="btn-like" onclick="count('${doc.id}')" id="icon-like"><i class="fas fa-heartbeat iconPost"></i></button></div>
     </div>
     `;
@@ -65,17 +65,17 @@ function eliminar(id) {
 }
 
 //Funcion editar
-function editar(creator, userText) {
+function editar(id, userText) {
   document.getElementById('userInput').value = userText;
   let boton = document.getElementById('publishBtn');
   boton.innerHTML = 'Editar';
   //ejecutar funcion boton editar
   boton.onclick = function () {
-    let messageRef = db.collection("messages").doc(creator);
+    let messageRef = db.collection("messages").doc(id);
     //cambiar y editar el mensaje del usuario, y guardadas en variables
     let userText = document.getElementById('userInput').value;
     return messageRef.update({
-        text: userText,
+      text: userText,
       })
       .then(function () {
         console.log("editado!");
