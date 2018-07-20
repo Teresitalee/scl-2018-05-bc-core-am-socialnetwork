aab4e9dbf754482fd7163d2d0cb71782f2892907
Natalia <nataliamp87@gmail.com>
2018-07-19 23:29:57 -0400

    elimine una funcion de likes que estaba haciendo

--- assets/js/database.js
+++ assets/js/database.js
@@ -91,39 +91,8 @@ function editar(id, userText) {
   };
 }
 
-//Contador de likes y guardarlo a DB
-
 //Boton me gusta
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// Keeps track of the length of the 'likes' child list in a separate property.
-exports.countlikechange = functions.database.ref('/posts/{postid}/likes/{likeid}').onWrite(
-    (change) => {
-      const collectionRef = change.after.ref.parent;
-      const countRef = collectionRef.parent.child('likes_count');
-
-      let increment;
-      if (change.after.exists() && !change.before.exists()) {
-        increment = 1;
-      } else if (!change.after.exists() && change.before.exists()) {
-        increment = -1;
-      } else {
-        return null;
-      }
-
-      // Return the promise from countRef.transaction() so our function
-      // waits for this async event to complete before it exits.
-      return countRef.transaction((current) => {
-        return (current || 0) + increment;
-      }).then(() => {
-        return console.log('Counter updated.');
-      });
-    });
-
-/*
 function saveLikeToDB() {
   const db = firebase.firestore();
   let likes = document.getElementById('like-post').value;
@@ -164,4 +133,3 @@ function countLikeInDb() {
     });
 
 }
-*/
